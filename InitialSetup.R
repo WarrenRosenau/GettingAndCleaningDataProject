425fe7244fa1f832b1883942117799c067070453
WarrenRosenau <warrenrosenau@gmail.com>
2016-09-28 14:35:59 -0400

    Setup file

---
+++ InitialSetup.R
@@ -0,0 +1,9 @@
+# Here are the data for the project:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+projectDatasetUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+projectDatasetFile <- "./getdata_projectfiles_UCI_HAR_Dataset.zip"
+
+download.file(projectDatasetUrl,projectDatasetFile, method = "curl")
+
+unzip(projectDatasetFile)
